337fefbd8fe034e94deca56e7b55ee6779db47fe
Jose Garcia <garciajd90@gmail.com>
2015-11-18 22:46:00 -0700

    don't actually need a server for this (for now anyway)

--- main.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var serveStatic = require('serve-static');
-
-var serve = serveStatic('build', {
-  'index': ['index.html']
-});
-var app = express();
-app.use(serve);
-app.listen(8080);
