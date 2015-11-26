79bc61e6948aa0633c0a16376feb2d1f62017474
Jose Garcia <garciajd90@gmail.com>
2015-11-26 00:46:54 -0700

    move card preparation to CardStore

---
+++ src/js/constants/OrderConstants.js
@@ -0,0 +1,30 @@
+var OrderConstants = {
+  Class: {
+    'Druid': 0,
+    'Hunter': 1,
+    'Mage': 2,
+    'Paladin': 3,
+    'Priest': 4,
+    'Rogue': 5,
+    'Shaman': 6,
+    'Warlock': 7,
+    'Warrior': 8,
+    'Neutral': 9
+  },
+
+  Rarity: {
+    'Free': 0,
+    'Common': 1,
+    'Rare': 2,
+    'Epic': 3,
+    'Legendary': 4
+  },
+
+  Type: {
+    'Weapon': 0,
+    'Spell': 1,
+    'Minion': 2
+  }
+};
+
+module.exports = OrderConstants;
--- src/js/stores/CardStore.js
+++ src/js/stores/CardStore.js
@@ -1,9 +1,49 @@
 var EventEmitter = require('events');
 var objectAssign = require('object-assign');
+var _ = require('lodash');
 
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 
-var CardStore = {};
+var OrderConstants = require('../constants/OrderConstants');
+
+var AllSets = require('../../static/AllSets');
+// Delete non-card sets
+delete AllSets['Credits'];
+delete AllSets['Debug'];
+delete AllSets['Hero Skins'];
+delete AllSets['Missions'];
+delete AllSets['System'];
+delete AllSets['Tavern Brawl'];
+
+// Attach set, ordered rarity, and 'Neutral' class to cards
+_.keys(AllSets).forEach(function(setName) {
+  AllSets[setName] = _.forEach(AllSets[setName], function(item) {
+    item.set = setName;
+    item.rarity_order = OrderConstants.Rarity[item.rarity];
+    item.type_order = OrderConstants.Type[item.type];
+    if (!item.playerClass) {
+      item.playerClass = 'Neutral';
+    }
+  });
+});
+
+var _cardMap = {};
+var _cards = _(AllSets)
+  .map()
+  .flatten()
+  .filter(function(card) {
+    if (!!card.collectible && card.type !== 'Hero') {
+      _cardMap[card.id] = card;
+      return true;
+    }
+  })
+  .value();
+
+var CardStore = {
+  getAllCards: function() {
+    return _cards;
+  },
+};
 
 var dispatchCallback = function(action) {
 };
