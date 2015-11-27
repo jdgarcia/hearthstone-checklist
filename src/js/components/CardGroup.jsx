f79dbe44c336d91a3c002fe51bacabd64bb0d026
Jose Garcia <garciajd90@gmail.com>
2015-11-26 20:53:48 -0700

    group stats by rarity

--- src/js/components/CardGroup.jsx
+++ src/js/components/CardGroup.jsx
@@ -1,23 +1,46 @@
 var React = require('react');
 var _ = require('lodash');
 
+var OrderConstants = require('../constants/OrderConstants');
 var Card = require('./Card');
 
 var CardGroup = React.createClass({
   render: function() {
-    var total = 0;
-    var owned = 0;
+    var groupName = this.props.groupName;
+
+    var rarities = ['All'].concat(_.keys(OrderConstants.Rarity));
+    var stats = {};
+    _.forEach(rarities, function(rarity) {
+      stats[rarity] = { total: 0, owned: 0 };
+    });
+
     var cards = _.map(this.props.cards, function(card) {
-      total += card.target;
-      owned += card.owned;
+      stats.All.total += card.target;
+      stats.All.owned += card.owned;
+      stats[card.rarity].total += card.target;
+      stats[card.rarity].owned += card.owned;
+
       return (
         <Card card={card} key={card.id} />
       );
     });
 
+    var rarityStats = _.map(stats, function(stat, rarity) {
+      if (stat.total === 0) {
+        return null;
+      }
+
+      return (
+        <div key={groupName + '-' + rarity}>
+          {rarity} {stat.owned}/{stat.total} ({Math.round(stat.owned/stat.total*100)}%)
+        </div>
+      );
+    });
+
     return (
       <div>
-        {this.props.groupName} {owned}/{total} ({Math.round(owned/total*100)}%)
+        <h2>{groupName}</h2>
+        {rarityStats}
         <table>
           <tbody>
             {cards}
